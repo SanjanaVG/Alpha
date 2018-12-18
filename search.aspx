5c124743e533f8dee79fcddf244ec27c148e7b7b
SanjanaVG <45872483+SanjanaVG@users.noreply.github.com>
2018-12-18 18:15:49 +0530

    Add files via upload

---
+++ search.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="search.aspx.cs" Inherits="emppro.WebForm2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    </asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+          <h4>Welcome&nbsp;
+              <asp:Label ID="Label3" runat="server"></asp:Label>
+          </h4>
+     <div class="b"><a href="Loginpage.aspx">Logout</a><br /></div>
+    <div class="a">
+          <h4>Employee search</h4>
+    <asp:Label ID="Label2" runat="server" Text="Search By"></asp:Label>
+    <asp:DropDownList ID="DropDownList1" runat="server" TabIndex="1">
+        <asp:ListItem>Employee Id</asp:ListItem>
+        <asp:ListItem>Employee Name</asp:ListItem>
+            </asp:DropDownList>
+    <asp:Label ID="Label1" runat="server" Text="Contains"></asp:Label>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <asp:Button ID="Button1" runat="server" Text="Search" OnClick="Button1_Click" />
+        </div>
+       
+        <asp:DataGrid ID="DataGrid1" runat="server"  Width="253px" >
+
+        </asp:DataGrid>
+         
+          <br />
+          <asp:Label ID="lblMsg" runat="server"></asp:Label>
+        
+          <br />
+          <br />
+   
+          </asp:Content>
---
+++ search.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace emppro
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Label3.Text = Request.QueryString["Parameter"].ToString();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           
+            string ConnectionString;
+            SqlConnection MyCon;
+            ConnectionString = "server=WSLKCMP5F-582;user id=sa;password=slk@SOFT;database=Adventure Works";
+            MyCon = new SqlConnection(ConnectionString);
+      
+            try
+            {
+            
+                if (DropDownList1.SelectedIndex == 1)
+                {
+                    
+                    int Val = int.Parse(TextBox1.Text);
+                    string Query = "select EmpId,EmpName,DOB,Gender,ContactNo,EmailId,DOJ,Department,Designation from Tbl_Employee where EmpId ='" + Val + "'";
+                    SqlCommand cmd = new SqlCommand(Query, MyCon);
+                   SqlDataAdapter sde = new SqlDataAdapter();
+                   DataSet ds = new DataSet();
+                   sde.Fill(ds);
+                    DataTable dt = ds.Tables["tabl"];
+                   DataGrid1.DataSource = ds;
+                   DataGrid1.DataBind();
+                }
+                else if (DropDownList1.SelectedIndex == 2)
+                {
+                    string Val = TextBox1.Text;
+                    string Query = "select EmpId,EmpName,DOB,Gender,ContactNo,EmailId,DOJ,Department,Designation from Tbl_Employee where EmpName ='" + Val + "'";
+                    SqlDataAdapter sde = new SqlDataAdapter(Query, MyCon);
+                        DataSet ds = new DataSet();
+                        sde.Fill(ds);
+                        DataGrid1.DataSource = ds;
+                        DataGrid1.DataBind();
+                 }                
+              }
+            catch (Exception ee)
+            {
+                lblMsg.Text = "the error is" + ee;
+            }
+            finally { MyCon.Close(); }
+          
+            }
+
+    
+    }
+    }
+
